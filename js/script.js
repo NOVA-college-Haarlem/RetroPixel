68983b91a7ffad6cdfbc93d46e585fca7691613c
David van Oudheusden <doudheusden@novacollege.nl>
2026-09-02 10:22:55 +0200

    Init commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log("RetroPixel is geladen!");
+
+let winkelnaam = "RetroPixel";
+let jaarOpgericht = 2018;
+
+console.log(winkelnaam + " bestaat sinds " + jaarOpgericht);
+
+
+let naamCollege = "Nova College";
+
+naamCollege = "ROC van Amsterdam";
+
+let aantal_winkels = 4;
+
+
+console.log("Er zijn " + aantal_winkels + " winkels in " + stad + ".");
+
+const stad = "amsterdam";
