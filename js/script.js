3f02cd9a7c70aeca6198ff3cccf088fd0332bee9
David van Oudheusden <doudheusden@novacollege.nl>
2026-09-15 19:48:46 +0200

    Refactor HTML structure and add CSS styles for improved layout and design

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
-console.log("RetroPixel is geladen!");
+// console.log("RetroPixel is geladen!");
 
-let winkelnaam = "RetroPixel";
-let jaarOpgericht = 2018;
+// let winkelnaam = "RetroPixel";
+// let jaarOpgericht = 2018;
 
-console.log(winkelnaam + " bestaat sinds " + jaarOpgericht);
+// console.log(winkelnaam + " bestaat sinds " + jaarOpgericht);
 
+// let naamCollege = "Nova College";
 
-let naamCollege = "Nova College";
+// naamCollege = "ROC van Amsterdam";
 
-naamCollege = "ROC van Amsterdam";
+// let aantal_winkels = 4;
 
-let aantal_winkels = 4;
+// let title = document.querySelector("h1");
 
+// title.textContent = "Welkom bij RetroPixel";
+// title.style.color = "goldenrod";
 
-console.log("Er zijn " + aantal_winkels + " winkels in " + stad + ".");
+// const heroKnop = document.querySelector("#hero-knop");
+// heroKnop.textContent = "Bekijk de laatste games";
+// heroKnop.style.backgroundColor = "limegreen";
 
-const stad = "amsterdam";
+// const contact = document.querySelector("#contact p");
+
+// contact.textContent = "testing my console";
+
+// console.log(contact);
+
+// const header = document.querySelector(".header");
+
+// header.style.backgroundColor = "blue";
+
+// header.textContent = "NICE BLUE STUFF";
+
+// heroKnop.addEventListener("click", function () {
+//     alert("Pas op!");
+//     confirm("Weet je het zeker?");
+//     prompt("Wat is je leeftijd?");
+// });
